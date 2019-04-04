a94e0bb64b28d2c41ddf4ca3dfef79eb803d4dd7
Helena Paixao <hp.helenapaixao@gmail.com>
2019-04-03 20:23:23 -0400

    Editando a pagina cliente

--- loja-ht-admin/src/utils/HTTPRequestUtil.js
+++ loja-ht-admin/src/utils/HTTPRequestUtil.js
@@ -1,37 +1,49 @@
-const API_URL = "https://loja-ht-api-t23.herokuapp.com/"
-import axios from 'axios'
+const API_URL = "https://loja-ht-api-t23.herokuapp.com/";
+import axios from "axios";
 
-export default{
-    // DEPARTAMENTOS
-    ////////////////////////////////
-    async getDepartamentos(){
-        return axios.get(API_URL + 'departamentos')
-        .then(response => response.data)
-    },
+export default {
+  // DEPARTAMENTOS
+  ////////////////////////////////
+  async getDepartamentos() {
+    return axios.get(API_URL + "departamentos").then(response => response.data);
+  },
 
-    async adicionarDepartamento(departamento){
-        return axios.post(API_URL + 'departamentos', departamento)
-        .then(response => response.data)
-    },
+  async adicionarDepartamento(departamento) {
+    return axios
+      .post(API_URL + "departamentos", departamento)
+      .then(response => response.data);
+  },
 
-    async editarDepartamento(departamento){
-        return axios.put(API_URL + 'departamentos', departamento)
-        .then(response => response.data)
-    },    
-    //////////USUARIOS////////////
-    async buscarUsuarios(){
-        return axios.get(API_URL + 'usuarios')
-        .then(response => response.data)
-    },
+  async editarDepartamento(departamento) {
+    return axios
+      .put(API_URL + "departamentos", departamento)
+      .then(response => response.data);
+  },
+  //////////USUARIOS////////////
+  async buscarUsuarios() {
+    return axios.get(API_URL + "usuarios").then(response => response.data);
+  },
 
-    async salvarUsuario(usuario){
-        return axios.post(API_URL + 'usuarios', usuario)
-        .then(response => response.data)
-    },
+  async salvarUsuario(usuario) {
+    return axios
+      .post(API_URL + "usuarios", usuario)
+      .then(response => response.data);
+  },
 
-    async editarUsuario(usuario){
-        return axios.put(API_URL + 'usuarios', usuario)
-        .then(response => response.data)
-    },    
+  async editarUsuario(usuario) {
+    return axios
+      .put(API_URL + "usuarios", usuario)
+      .then(response => response.data);
+  },
+
+  ///CLIENTES
+
+  async getClientes() {
+    return axios.get(API_URL + "clientes").then(response => response.data);
+  },
+  async adicionarCliente(cliente) {
+    return axios
+      .post(API_URL + "clientes", cliente)
+      .then(response => response.data);
+  }
 };
-
--- loja-ht-admin/src/views/Clientes.vue
+++ loja-ht-admin/src/views/Clientes.vue
@@ -1,15 +1,102 @@
 <template>
-    <div class="clientes">
-        CLIENTES
-    </div>
+  <div class="clientes">
+    <v-flex xs13 sm7 offset-sm3>
+      <v-card>
+        <v-text-field
+          class="mx-3"
+          flat
+          label="Buscar"
+          prepend-inner-icon="search"
+          v-model="buscar"
+          clearable
+          @click:clear="clearSearch"
+        ></v-text-field>
+        <v-spacer></v-spacer>
+      </v-card>
+    </v-flex>
+    <v-list three-line>
+      <v-subheader>{{ title }}</v-subheader>
+      <template v-for="(cliente) in filteredItems">
+        <v-list-tile :key="cliente.nome">
+          <v-list-tile-content>
+            <v-list-tile-title v-html="cliente.nome"></v-list-tile-title>
+
+            <v-list-tile-title v-html="cliente.cpf"></v-list-tile-title>
+
+            <v-list-tile-title v-html="cliente.dataNascimento"></v-list-tile-title>
+            <v-list-tile-title v-html="cliente.usurname"></v-list-tile-title>
+          </v-list-tile-content>
+          <v-list-tile-action>
+            <v-layout>
+              <v-flex xs12 class="my-4">
+                <v-btn icon ripple @click="inativar(cliente)">
+                  <v-icon>not_interested</v-icon>
+                </v-btn>
+              </v-flex>
+            </v-layout>
+          </v-list-tile-action>
+        </v-list-tile>
+      </template>
+    </v-list>
+  </div>
 </template>
 
 <script>
+import HTTPRequestUtil from "@/utils/HTTPRequestUtil";
 export default {
+  data: () => ({
+    selected: [2],
+    buscar: "",
+    clientes: [],
+    title: "Lista de Clientes"
+  }),
 
-}
+  mounted: function() {
+    this.getClientes();
+  },
+  methods: {
+    inativar(pessoa) {
+      alert("Método Inativar clicado!" + JSON.stringify(pessoa));
+    },
+    toggle(index) {
+      const i = this.selected.indexOf(index);
+
+      if (i > -1) {
+        this.selected.splice(i, 1);
+      } else {
+        this.selected.push(index);
+      }
+    },
+
+    getClientes() {
+      HTTPRequestUtil.getClientes().then(clientes => {
+        this.clientes = clientes;
+      });
+    },
+
+    limparDados() {
+      this.buscar = "";
+    }
+  },
+  computed: {
+    filteredItems() {
+      return _.orderBy(
+        this.clientes.filter(cliente => {
+          if (!this.buscar) return this.clientes;
+          return (
+            cliente.nome.toLowerCase().includes(this.buscar.toLowerCase()) ||
+            cliente.cpf.toLowerCase().includes(this.buscar.toLowerCase()) ||
+            cliente.datadeNascimento
+              .toLowerCase()
+              .includes(this.buscar.toLowerCase()) ||
+            cliente.usuario.toLowerCase().includes(this.buscar.toLowerCase())
+          );
+        })
+      );
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>
