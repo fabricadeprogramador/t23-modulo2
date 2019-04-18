1a8016f4910e5f6ff4e8de330f236ed6ac1ec5f5
thiagoskill <thiagoskill@gmail.com>
2019-04-17 20:18:41 -0400

    Tela Home Pronta

--- loja-ht-admin/src/views/Home.vue
+++ loja-ht-admin/src/views/Home.vue
@@ -1,7 +1,28 @@
 <template>
-  <div class="home">
-    HOME
-  </div>
+
+<v-container grid-list-md>
+<v-layout row wrap>
+  <v-flex xs12>
+  <v-card-text>
+    <div class="text-xs-center">
+          <h1>Bem vindo</h1>
+        </div>
+  </v-card-text>
+  </v-flex>
+</v-layout>
+
+<v-layout align-center justify-space-around row fill-height>
+  <v-flex>
+  <v-img :src="require('@/assets/img1.jpg')" aspect-ratio="1"></v-img>
+  </v-flex>  
+  <v-flex>
+    <v-img :src="require('@/assets/img2.jpg')" aspect-ratio="1"></v-img>
+  </v-flex>
+  <v-flex>
+    <v-img :src="require('@/assets/img3.jpg')"></v-img>
+  </v-flex>
+</v-layout>
+</v-container>
 </template>
 
 <script>
@@ -9,3 +30,7 @@
 
   }
 </script>
+
+
+
+
